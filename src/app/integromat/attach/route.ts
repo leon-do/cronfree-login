202cbfda0facad5be8d479bfd145ed28fc7d7def
Leon Do <leondo@Leons-MacBook-Pro.local>
2023-08-21 18:48:15 -0400

    integromat init

---
+++ src/app/integromat/attach/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.SUPABASE_SERVICE_ROLE as string,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+    },
+  }
+);
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    console.log("/integromat/attach", body);
+    return NextResponse.json({ success: true });
+
+    // if (!body.license_key)
+    //   return NextResponse.json([{ id: false }], {
+    //     status: 401,
+    //     statusText: "Missing License Key",
+    //   });
+
+    // // query supabase for user
+    // const { data, error } = await supabase
+    //   .from("account")
+    //   .select("*")
+    //   .eq("license_key", body.license_key);
+    // if (error)
+    //   return NextResponse.json(
+    //     { success: false },
+    //     { status: 401, statusText: "Invalid License Key" }
+    //   );
+
+    // // check if the user has an account
+    // if (data.length === 1) return NextResponse.json([{ id: true }]);
+
+    // // if no user then unauthorized
+    // return NextResponse.json(
+    //   { success: false },
+    //   { status: 401, statusText: "Unauthorized" }
+    // );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false },
+      { status: 401, statusText: "Unauthorized" }
+    );
+  }
+}
