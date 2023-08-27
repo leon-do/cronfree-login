deb5815fe986c835fceb6b13bccb02cc6855fd4e
leon-do <leondo123@gmail.com>
2023-08-26 21:57:47 -0400

    type bool

--- src/app/zapier/performList/route.ts
+++ src/app/zapier/performList/route.ts
@@ -43,5 +43,5 @@ export async function POST(req: NextRequest) {
         status: 401,
       }
     );
-  return NextResponse.json([{ success: "true" }]);
+  return NextResponse.json([{ success: true }]);
 }
